d4f4f24d1b8e26151e220fd211ada3a50ea06575
M Zia Khan <mzkhan@netlink.com>
2020-04-05 13:47:16 +0530

    First Commit

---
+++ src/LambdaExpressionExamples/ComparatorImplementationByLambda.java
@@ -0,0 +1,27 @@
+package LambdaExpressionExamples;
+
+import java.util.Comparator;
+
+public class ComparatorImplementationByLambda {
+
+    public static void main(String[] args) {
+
+        //legacy way prior java 8
+        Comparator<Integer> comparator =new
+                Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer o1, Integer o2) {
+                        return o1.compareTo(o2);
+                    }
+                };
+        System.out.println("before java 8:"+ comparator.compare(32,76));
+    //Way 1
+        Comparator<Integer> comparator1=(Integer i1, Integer i2)->{return i1.compareTo(i2);};
+        System.out.println("After java 8:"+ comparator.compare(32,76));
+
+    //Way 2
+        Comparator<Integer> comparator2=(i1,i2)-> i1.compareTo(i2);
+        System.out.println("After java 8:"+ comparator.compare(32,76));
+
+    }
+}
---
+++ src/LambdaExpressionExamples/RunnableImplementationByLambda.java
@@ -0,0 +1,28 @@
+package LambdaExpressionExamples;
+
+public class RunnableImplementationByLambda {
+
+    public static void main(String[] args) {
+        //Legacy prior java 8
+
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside legacy implemmentation prior java 8");
+            }
+        };
+    new Thread(runnable).start();
+        //New Implementation with Lambda Expression java 8
+        //1
+        Runnable runnable1=()->{System.out.println("Inside new implemmentation with java 8");};
+        new Thread(runnable1).start();
+
+        //2
+        Runnable runnable2=()-> System.out.println("Inside new implemmentation with java 8-2");
+        new Thread(runnable2).start();
+
+        //3
+        new Thread(()-> System.out.println("Inside new implemmentation with java 8-3")).start();
+
+    }
+}
